5fc04be514adf1ce30742c16c38794d023f4712b
Sergio <serarroy@ucm.es>
2022-11-02 15:03:22 +0100

    funny-jokes finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import RotateLoader from 'react-spinners/RotateLoader';
 import './App.css';
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [jokeAnswer, setJokeAnswer] = useState('');
+  const [jokeQuestion, setJokeQuestion] = useState('');
+
+  
+  const getJoke = async() =>{
+    setTimeout(() => {
+      setIsLoading(false);
+      fetch('https://official-joke-api.appspot.com/jokes/random')
+      .then(res => res.json()).then(data => {
+        setJokeQuestion(data.setup);
+        setJokeAnswer(data.punchline);
+      })
+    }, 3000);
+  }
+
+  useEffect(() => {
+    getJoke()
+  }, [])
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='title'>Funny Jokes</h1>
+      {
+        isLoading ? <RotateLoader size={15} className='rotate-loader'/>: null
+      }
+      <div className='jokes-container'>
+        <div className='jokes'>
+          <div className='joke'>{jokeQuestion}</div>
+          <div className='joke'>{jokeAnswer}</div>
+        </div>
+        <div>
+          <button className='joke-button' onClick={getJoke}>Tell me a joke</button>
+        </div>
+      </div>
     </div>
   );
 }
